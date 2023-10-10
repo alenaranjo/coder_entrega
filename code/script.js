6bc7a23c4ccff2666e2b0dfe4c430be52e92eca1
alenaranjo <alenaranjo777@hotmail.com>
2023-10-10 13:24:07 -0600

    changes per feedback from tutor

--- code/script.js
+++ code/script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const products = [
+        {id: 'arroz',nombre: 'Arroz',precio: 1500,},
+        {id: 'cereal',nombre: 'Cereal',precio: 1000,},
+        {id: 'leche',nombre: 'Leche',precio: 800,},
+        {id: 'pastas',nombre: 'Pastas',precio: 900,},
+        {id: 'galletas',nombre: 'Galletas',precio: 970,},
+        {id: 'refresco',nombre: 'Refresco',precio: 700,},
+    ];
+
     const selectedProducts = new Map();
     const productButtons = document.querySelectorAll('.producto');
     const cart = document.getElementById('cart');
@@ -6,12 +15,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const iva = document.getElementById('iva');
     const totalConIVA = document.getElementById('totalConIVA');
     const clearCartButton = document.getElementById('clear-cart');
-    const editCartButton = document.getElementById('edit-cart');
 
-    let edicionHabilitada = false; // Variable para habilitar la edición
-
-    // Cargar datos del carrito desde sessionStorage al cargar la página
-    const savedCart = sessionStorage.getItem('cart');
+    // Cargar datos del carrito desde localStorage al cargar la página
+    const savedCart = localStorage.getItem('cart');
     if (savedCart) {
         const parsedCart = JSON.parse(savedCart);
         parsedCart.forEach(item => {
@@ -20,55 +26,45 @@ document.addEventListener("DOMContentLoaded", function () {
         renderCart();
     }
 
+    // Función para calcular el precio total de un producto
+    function calcularPrecioTotal(product) {
+        return product.cantidad * product.precio;
+    }
+
     // Función para renderizar el carrito
     function renderCart() {
         cart.innerHTML = '';
 
         let totalPrecioSinIVA = 0;
 
-        // Convertir el mapa de productos a una matriz para guardar en sessionStorage
+        // Actualizar el precio de cada producto en el carrito
+        selectedProducts.forEach((product) => {
+            product.precioTotal = calcularPrecioTotal(product);
+        });
+
+        // Convertir el mapa de productos a una matriz para guardar en localStorage
         const cartArray = Array.from(selectedProducts.values());
 
-        // Guardar el carrito actualizado en sessionStorage
-        sessionStorage.setItem('cart', JSON.stringify(cartArray));
+        // Guardar el carrito actualizado en localStorage
+        localStorage.setItem('cart', JSON.stringify(cartArray));
 
         cartArray.forEach((product) => {
             const row = document.createElement('div');
             row.classList.add('cart-item');
             row.innerHTML = `
-                <p>${product.nombre}, Cantidad: <span class="${edicionHabilitada ? 'editable' : ''}" contenteditable="${edicionHabilitada ? 'true' : 'false'}">${product.cantidad}</span>, Precio: $${(product.cantidad * product.precio).toFixed(2)}</p>
+                <div class="producto-card">
+                    <h3>${product.nombre}</h3>
+                    <p class="precio">Total: $${(product.precioTotal).toFixed(2)}</p>
+                    <p>Cantidad: <span>${product.cantidad}</span></p>
+                    <button class="btn_clear" onclick="removeItem('${product.id}')">Borrar</button>
+                    <button class="btn_clear" onclick="decreaseQuantity('${product.id}')">Quitar uno</button>
+                    <button class="btn_clear" onclick="addQuantity('${product.id}')">Agregar otro</button>
+                </div>
             `;
-
-            const cantidadEditable = row.querySelector('span');
-
-            // Escucha el evento 'click' para seleccionar todo el texto en el campo de cantidad
-            cantidadEditable.addEventListener('click', () => {
-                cantidadEditable.style.fontSize = '15px'; // Aumenta el tamaño de fuente al hacer clic
-                const range = document.createRange();
-                range.selectNodeContents(cantidadEditable);
-                const selection = window.getSelection();
-                selection.removeAllRanges();
-                selection.addRange(range);
-            });
-
-            // Escucha el evento 'input' para actualizar la cantidad y el precio cuando se edita
-            cantidadEditable.addEventListener('input', () => {
-                const nuevaCantidad = parseInt(cantidadEditable.textContent);
-                if (!isNaN(nuevaCantidad) && nuevaCantidad > 0) {
-                    product.cantidad = nuevaCantidad;
-                    // Actualiza el precio
-                    cantidadEditable.parentElement.lastElementChild.textContent = `$${(product.cantidad * product.precio).toFixed(2)}`;
-                }
-                // Actualiza la lista de productos seleccionados
-                renderCart();
-
-                // Restaurar el tamaño de fuente original (12px) después de cambiar la cantidad
-                cantidadEditable.style.fontSize = '12px';
-            });
 
             cart.appendChild(row);
 
-            totalPrecioSinIVA += product.cantidad * product.precio;
+            totalPrecioSinIVA += product.precioTotal;
         });
 
         const totalPrecioConIVA = totalPrecioSinIVA * 1.13;
@@ -85,42 +81,63 @@ document.addEventListener("DOMContentLoaded", function () {
             const productPrecio = parseFloat(button.getAttribute('data-precio'));
 
             if (selectedProducts.has(productId)) {
-                // Si el producto ya está en el carrito, aumenta la cantidad y actualiza el precio
+                // Si el producto ya está en el carrito, aumenta la cantidad
                 const product = selectedProducts.get(productId);
                 product.cantidad++;
-                product.precio += productPrecio;
             } else {
                 // Si es un nuevo producto, crea una entrada en el carrito
                 selectedProducts.set(productId, {
                     id: productId,
-                    nombre: productId.charAt(0).toUpperCase() + productId.slice(1), // Primera letra en mayúscula
+                    nombre: button.previousElementSibling.previousElementSibling.textContent.trim(),
                     cantidad: 1,
                     precio: productPrecio,
                 });
             }
 
-            // Actualiza la lista de productos seleccionados
             renderCart();
         });
     });
 
+    // Función para remover un producto del carrito
+    window.removeItem = function (productId) {
+        if (selectedProducts.has(productId)) {
+            selectedProducts.delete(productId);
+        }
+        // Actualiza la lista de productos seleccionados
+        renderCart();
+    };
+
+    // Función para disminuir la cantidad de un producto en el carrito
+    window.decreaseQuantity = function (productId) {
+        if (selectedProducts.has(productId)) {
+            const product = selectedProducts.get(productId);
+            if (product.cantidad > 1) {
+                product.cantidad--;
+            } else {
+                selectedProducts.delete(productId);
+            }
+        }
+        
+        renderCart();
+    };
+
+    // Función para aumentar la cantidad de un producto en el carrito
+    window.addQuantity = function (productId) {
+        if (selectedProducts.has(productId)) {
+            const product = selectedProducts.get(productId);
+            product.cantidad++;
+        }
+        
+        renderCart();
+    };
+
     // Manejar el botón de limpiar carrito
     clearCartButton.addEventListener('click', () => {
         selectedProducts.clear();
-        sessionStorage.removeItem('cart'); // Limpiar también los datos de sessionStorage
+        localStorage.removeItem('cart'); 
+       
         renderCart();
     });
 
-    // Manejar el botón de editar lista
-    editCartButton.addEventListener('click', () => {
-        edicionHabilitada = !edicionHabilitada; // Cambiar el estado de edición
-        const cantidadEditable = document.querySelectorAll('.cart-item span[contenteditable="false"]');
-        cantidadEditable.forEach(editable => {
-            editable.contentEditable = edicionHabilitada ? 'true' : 'false'; // Habilitar o deshabilitar la edición
-            if (!edicionHabilitada) {
-                editable.textContent = ''; // Limpia el contenido inicial si se deshabilita la edición
-                cantidadEditable.style.fontSize = '12px'; // Restaurar el tamaño de fuente original
-            }
-        });
-    });
+    renderCart();
 });
